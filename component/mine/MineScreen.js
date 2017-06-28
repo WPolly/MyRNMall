e820ab9886514528a429cbaf313bb48377ebf0c8
lishan <643633832@qq.com>
2017-06-28 23:43:54 +0800

    Add header view on MineScreen.

--- component/mine/MineScreen.js
+++ component/mine/MineScreen.js
@@ -1,21 +1,32 @@
 /**
  * Created by Administrator on 2017/6/15.
  */
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
     StyleSheet,
     Text,
-    View
+    View,
+    Image
 } from 'react-native';
 
-var MineScreen = React.createClass({
+const Dimensions = require('Dimensions');
+const {width} = Dimensions.get('window');
+
+const MineScreen = React.createClass({
 
     render() {
         return (
             <View style={styles.container}>
-                <Text style={styles.welcome}>
-                    哈哈哈
-                </Text>
+                <View style={styles.userProfileViewStyle}>
+                    <Image source={{uri: 'dog'}} style={styles.avatarStyle}/>
+                    <Text style={{color: 'white', fontSize: 18}}>美团外卖</Text>
+                    <Image source={{uri: 'avatar_vip'}} style={{width: 20, height: 20}}/>
+                    <Image source={{uri: 'icon_cell_right_arrow'}}
+                           style={{width: 8, height: 12, position: 'absolute', right: 10}}/>
+                </View>
+
+                <View style={styles.userDataView}>
+                </View>
             </View>
         );
     }
@@ -23,17 +34,31 @@ var MineScreen = React.createClass({
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        justifyContent: 'center',
+        height: 150,
+        backgroundColor: '#fd4b1f',
+        justifyContent:'space-between',
+    },
+    userProfileViewStyle: {
+        flexDirection: 'row',
+        marginTop:20,
+        marginLeft:10,
         alignItems: 'center',
-        backgroundColor: '#F5FCFF',
     },
-    welcome: {
-        fontSize: 20,
-        textAlign: 'center',
-        margin: 10,
-        color: 'blue',
+    avatarStyle: {
+        width: 75,
+        height: 75,
+        borderRadius: 37,
+        borderWidth: 1,
+        borderColor: '#feffd7',
+        marginRight: 4,
     },
+    userDataView: {
+        width: width,
+        height: 42,
+        position: 'absolute',
+        bottom: 0,
+        backgroundColor:'rgba(255,255,255,0.4)',
+    }
 });
 
 module.exports = MineScreen;
